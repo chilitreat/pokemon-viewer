6a29f5ecf16be497c5a905d9c56c5d35715467fb
shirata <dendenden00@gmail.com>
2020-01-07 01:16:39 +0900

    vuetifyの型を参照する

--- client/nuxt.config.js
+++ client/nuxt.config.js
@@ -1,5 +1,8 @@
 export default {
-  buildModules: ['@nuxt/typescript-build'],
+  buildModules: [
+    '@nuxt/typescript-build',
+    '@nuxtjs/vuetify'
+  ],
   plugins: [
     '~/plugins/composition-api',
     '~/plugins/provide-apollo-client'
